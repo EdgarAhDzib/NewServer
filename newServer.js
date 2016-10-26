0c8c3f0199fa6f90d9ab5a80fab3a2b74c2a81c1
Edgar Martin del Campo <ixmati@comcast.net>
2016-10-25 19:35:58 -0500

    Revised for texts to appear in browser

--- newServer.js
+++ newServer.js
@@ -1,26 +1,31 @@
 var http = require('http');
 var tweet = require('twitter');
 
-var PORT1 = 7000;
-var PORT2 = 7500;
-
-function handleRequest(request, response) {
-	response.end("It worked! Path: " + request.url);
-}
-
-var server = http.createServer(handleRequest);
+var PORTONE = 7000;
+var PORTTWO = 7500;
 
 var niceThings = ["Your career is about to launch!","Happy Days of the Dead!","The soundtrack kicks butt!"];
 var badThings = ["Your church makes me vomit!","Your costume direly needs accessories!","Get your clothes out of the suitcase already!"];
 var rand1 = Math.floor(Math.random() * niceThings.length);
 var rand2 = Math.floor(Math.random() * badThings.length);
 
-server.listen(PORT1, function(){
-	console.log(niceThings[rand1]);
+function handleRequestOne(request, response) {
+	response.end(niceThings[rand1]);
+}
+
+function handleRequestTwo(request, response) {
+	response.end(badThings[rand2]);
+}
+
+var serverOne = http.createServer(handleRequestOne);
+var serverTwo = http.createServer(handleRequestTwo);
+
+serverOne.listen(PORTONE, function(){
+	console.log("This server is listening on http://localhost:%s",PORTONE);
 });
 
-server.listen(PORT2, function(){
-	console.log(badThings[rand2]);
+serverTwo.listen(PORTTWO, function(){
+	console.log("This server is listening on http://localhost:%s",PORTTWO);
 });
 
 var twitKeys = require('./keys.js');
